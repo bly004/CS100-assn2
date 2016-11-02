8e4158d52dfcf551d78553b493a1c4f09ddcc559
bly004 <bly004@ucr.edu>
2016-11-02 10:42:43 +0000

    added code to find user info

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,33 @@
 #include "Program.h"
 
 #include <unistd.h>
+#include <pwd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fstream>
 
+
+
+
+
+int main() {
+    
+    //finding user login info
+    string user, info;
+    
+    char hostInfo[256];
+    struct passwd *u = getpwuid(getuid());
+    int h = gethostname(hostInfo, 256);
+    
+    if(u != 0 && h != -1) {
+        user = u->pw_name;
+        info = user + "@" + hostInfo + "$ ";
+    } else {
+        user = "anon_user";
+        info = user + "@host_name$ ";
+    }
+    
+    
+    
+    
+}
