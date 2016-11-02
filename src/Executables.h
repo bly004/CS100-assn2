905b3389d2b8ccda07092aa0275de0c0c4c69e4a
bly004 <bly004@ucr.edu>
2016-11-02 09:55:55 +0000

    moved folder

---
+++ src/Executables.h
@@ -0,0 +1,16 @@
+#ifndef EXECUTABLES_H
+#define EXECUTABLES_H
+
+#include "Input.h"
+
+class Executables : public Input {
+    protected:
+        int numArguments;
+    public:
+        Executables() { }
+        bool endOfLine() {
+            
+        }
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "AndConnector.h"
+#include "Arguments.h"
+#include "Connectors.h"
+#include "Executables.h"
+#include "Input.h"
+#include "InputComposite.h"
+#include "Line.h"
+#include "OrConnector.h"
+#include "Program.h"
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fstream>
+
