2c77a3e6c4578e1e7c4b66eee9be7bf2da0f1cd3
bly004 <bly004@ucr.edu>
2016-11-02 02:01:22 +0000

    adding src files

---
+++ src/Connectors.h
@@ -0,0 +1,14 @@
+#ifndef CONNECTORS_H
+#define CONNECTORS_H
+
+#include "Input.h"
+
+class Connectors : public Input {
+    public:
+        Connectors() { }
+        bool endOfLine() {
+            
+        }
+};
+
+#endif
