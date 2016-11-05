004a4822bd190fc93999b7d9abc11e6a2d2b7ff4
bly004 <bly004@ucr.edu>
2016-11-05 00:02:02 +0000

    modified test files

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -1,11 +1,37 @@
 #!/bin/sh
 
-ls #-a
-mkdir test #making test file
-echo hello
-clear
-ls -l
-git status # making sure branch is up-to-date
-ls; cd test #going into test file
-cd .. && clear # returning to parent directory and clearing terminal
-cal && date #prints out calendar, date, and time
+LS_CMD=./bin/ls
+ECHO_CMD=./bin/echo
+CAL_CMD=./bin/cal
+DATE_CMD=./bin/date
+TEST="# testing"
+
+
+# $1 = message of what we're testing
+# $2 = command to be executed
+function printcmd {
+    echo -e "$TEST $1 $2"
+    $2
+}
+
+echo -e "\n# testing ls\n"
+printcmd "No flags:" "$CMD1"
+printcmd "-a flag:" "$CMD1 -a # listing all files and directories"
+printcmd "-l flag:" "$CMD1 -l # long-listing format"
+printcmd "-R flag:" "$CMD1 -R # listing subdirectories recursively"
+
+
+echo -e "\n# testing echo\n"
+printcmd "a test message:" "$CMD2 \"this is a test\""
+printcmd "hello world!:" "$CMD2 \"hello world!\""
+
+
+echo -e "\n# testing calendar, date, and time\n"
+printcmd "calendar:" "$CAL_CMD # displays a calendar and the date of Easter"
+printcmd "calendar month for July 2010:" "$CAL_CMD 7 2010 # displays July 2010"
+printcmd "this year's calendar:" "$CAL_CMD -y # displays calendar for entire year"
+printcmd "today's date:" "$DATE_CMD %D # displays date in m/d/y format"
+printcmd "today's date in diff format:" "$DATE_CMD %b %d, %Y # displays date in MMM DD, YYYY format"
+printcmd "current time:" "$DATE_CMD %X # displays time in hh:mm:ss format"
+
+echo -e "\n# done testing\n"
--- tests/multi_command.sh
+++ tests/multi_command.sh
@@ -1,7 +1,34 @@
 #!/bin/sh
 
-ls && ls -a || ls -l
-ls -a; echo hello; mkdir test
-ls -a; echo hello && mkdir test || echo world; git status
-cd test; echo testing cd; cd .. && echo "returned to parent directory" || echo "returning to parent directory failed"
-cal && date; exit
+LS_CMD=./bin/ls
+ECHO_CMD=./bin/echo
+CAL_CMD=./bin/cal
+DATE_CMD=./bin/date
+TEST="# testing"
+
+# $1 = message of what we're testing
+# $2 = command to be executed
+function printcmd {
+    echo -e "$TEST $1 $2"
+    $2
+}
+
+
+echo -e "\n# testing multiple ls\n" 
+printcmd "ls and ls -a:" "$LS_CMD; $LS_CMD -a"
+printcmd "ls and ls -l:" "$LS_CMD && $LS_CMD -l"
+printcmd "ls -a and ls -l:" "$LS_CMD -a; $LS_CMD -l"
+printcmd "ls and ls -l or ls -a:" "$LS_CMD && $LS_CMD -a || $LS_CMD -l"
+
+
+echo -e "\n# testing multiple echo\n"
+printcmd "\"this is a test\":" "$ECHO_CMD this; $ECHO_CMD is && $ECHO_CMD a; $ECHO_CMD test"
+printcmd "\"hello world!\":" "$ECHO_CMD hello && $ECHO_CMD world!"
+printcmd "\"hello world!\" or \"hello there\":" "$ECHO_CMD hello; $ECHO_CMD world! || $ECHO_CMD there"
+
+
+echo -e "\n# testing calendar, date, and time"
+printcmd "calendar and date:" "$CAL_CMD && $DATE_CMD %D"
+printcmd "date and time:" "$DATE_CMD %D; $DATE_CMD %X"
+
+echo -e "\n# done testing\n"
