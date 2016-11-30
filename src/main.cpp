d89df5e1b81129ee1afb827228717dcc8204aada
bly004 <bly004@ucr.edu>
2016-11-30 08:09:08 +0000

    changed files

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <pwd.h>
 #include <cstdlib>
@@ -17,38 +18,73 @@ using namespace boost;
 bool execute(string);
 bool test(string);
 bool run(string);
+bool dir(string);
+string pwd = "";
+string oldpwd = "";
 
 int main(int argc, char** argv) {
-    //finding user login info
-    string user, info;
-    
-    char hostInfo[256];
-    struct passwd *u = getpwuid(getuid());
-    int h = gethostname(hostInfo, 256);
-    
-    if(u != 0 && h != -1) {
-        user = u->pw_name;
-        info = hostInfo;
-        info.append("@");
-        info.append(user);
-        info.append("$ ");
-    } else {
-        info = "user@host_name$ ";
-    }
     //string input = "";
     /*for(int i = 1; i < argc; i++) {//comment out if using user input
         input += argv[i];//comment out if using user input
     }//comment out for user input
     cout << "$ " << input << endl;*/
+    //pid_t pid;
+    pwd = getenv("PWD");
     while(1) {//use this for user input
         string input = "";
-        cout << info;//use this for user input
+        //cout << "$ ";//use this for user input
+        cout << getenv("PWD") << " ";
+        //cout << pwd << " ";
         getline(cin, input);//use this for user input instead of command line
         typedef tokenizer<char_separator<char> > tokenizer;
         char_separator<char> sep(";");
         tokenizer tkn(input, sep);
         for(tokenizer::iterator tok = tkn.begin(); tok != tkn.end(); tok++) {
             string currTok = *tok;
+            /*if(currTok.find("&") != string::npos && currTok.find("|") != string::npos) {
+                bool lPass;
+                bool inP;
+                bool pVal;
+                bool inA;
+                char_seperator<char> sep4("", "|&()", boost::keep_empty_tokens);
+                tokenizer tkn4(currTok, sep4);
+                for(tokenizer::iterator tok4 = tkn4.begin(), tok4 != tkn4.end(); tok4++) {
+                    if(*tok == "(") {
+                        inP = true;
+                        continue;
+                    }
+                    if(*tok == ")") {
+                        inP = false;
+                        pVal = lPass;
+                        continue;
+                    }
+                    if(*tok == "|") {
+                        inA = false;
+                        continue;
+                    }
+                    if(*tok == "&") {
+                        inA = true;
+                        continue;
+                    }
+                    if(tok4 == tkn4.begin()) {
+                        lPass = run(*tok4);
+                        continue;
+                    }
+                    if(lPass && !inA) {
+                        lPass = true;
+                        continue;
+                    }
+                    if(!lPass && !inA) {
+                        lPass = run(*tok4);
+                    }
+                    if(lPass && inA) {
+                        lPass = run(*tok4);
+                    }
+                    if(!lPass && inA) {
+                        lPass = false;
+                    }
+                }
+            }*/
             if(currTok.find("&") != string::npos) { //else if
                 bool passed = true;
                 char_separator<char> sep1("&");
@@ -88,18 +124,26 @@ int main(int argc, char** argv) {
 bool run(string temp) {
     bool ret;
     string temp2 = temp;
-    if( temp2.find("test") != string::npos ) {
-        while(temp2.at(temp2.size() - 1) == ' ') {
+    while(temp2.at(0) == ' ') {//changed
+            temp2 = temp2.substr(1);
+    }
+    while(temp2.at(temp2.size() - 1) == ' ') {//changed
             temp2 = temp2.substr(0, temp2.size() - 1);
-        }
+    }
+    if( temp2.find("test") != string::npos ) {
+        /*while(temp2.at(temp2.size() - 1) == ' ') {//changed
+            temp2 = temp2.substr(0, temp2.size() - 1);
+        }*/
         ret = test(temp2);
     } else if( temp2.find("[") != string::npos && temp2.find("]") != string::npos ) {
         temp2 = temp2.substr(0, temp2.find("]") - 1);
         ret = test(temp2);
+    } else if(temp2.find("cd") != string::npos) {
+        ret = dir(temp2);
     } else {
-        while(temp2.at(temp2.size() - 1) == ' ') {
+        /*while(temp2.at(temp2.size() - 1) == ' ') {//changed
             temp2 = temp2.substr(0, temp2.size() - 1);
-        }
+        }*/
         ret = execute(temp2);
     }
     return ret;
@@ -227,3 +271,122 @@ bool test(string temp) {
         }
     }
 }
+
+bool dir(string temp) {
+    if(temp.find("cd -") != string::npos) {
+        char* temp1 = getenv("OLDPWD");
+        if(temp1 == NULL) {
+            perror("getenv");
+            return false;
+        }
+        if(-1 == setenv("OLDPWD", getenv("PWD"), 1) ) {
+            perror("setenv");
+            return false;
+        } else {
+            oldpwd = getenv("OLDPWD");
+        }
+        if(-1 == setenv("PWD", temp1, 1) ) {
+            perror("setenv");
+            return false;
+        } else {
+            pwd = getenv("PWD");
+            chdir(pwd.c_str());
+        }
+        return true;
+    } else if(temp == "cd") {
+        char* temp1 = getenv("HOME");
+        if(temp1 == NULL) {
+            perror("getenv");
+            return false;
+        }
+        if(-1 == setenv("OLDPWD", getenv("PWD"), 1) ) {
+            perror("setenv");
+            return false;
+        } else {
+            oldpwd = getenv("OLDPWD");
+        }
+        if(-1 == setenv("PWD", temp1, 1) ) {
+            perror("setenv");
+            return false;
+        } else {
+            pwd = getenv("PWD");
+            chdir(pwd.c_str());
+        }
+        return true;
+    } else {
+        string temp1 = temp.substr(temp.find("cd") + 3);
+        temp1 = "/" + temp1;
+        char* temp2 = getenv("PWD");
+        temp1 = temp2 + temp1;
+        temp2 = &temp1[0];
+        /*if(temp2 == NULL) {
+            perror("getenv");
+            cout << "error end else" << endl;
+            return false;
+        }*/
+        char* temp3 = getenv("OLDPWD");
+        if(-1 == setenv("OLDPWD", getenv("PWD"), 1) ) {
+            perror("setenv");
+            return false;
+        } else {
+            oldpwd = getenv("OLDPWD");
+        }
+        if(-1 == setenv("PWD", temp2, 1) ) {
+            perror("setenv");
+            setenv("OLDPWD", temp3, 1);
+            oldpwd = getenv("OLDPWD");
+            return false;
+        } else {
+            pwd = getenv("PWD");
+            chdir(pwd.c_str());
+        }
+        return true;
+    }
+}
+
+//execute(*tok);
+                /*string temp = *tok;
+                if(temp.find(" ") == 0) {
+                    temp.erase(0,1);
+                }
+                if(temp.find("#") != string::npos) {
+                    int index = temp.find("#");
+                    temp = temp.substr(0, index);
+                }
+                if(temp.find(" ") != string::npos) {
+                    char_separator<char> sep_2(" ");
+                    tokenizer tkn_2(temp, sep_2);
+                    for(tokenizer::iterator tok_2 = tkn_2.begin(); tok_2 != tkn_2.end(); tok_2++) {
+                        string temp_2 = *tok_2;
+                        char *temp_1 = new char[temp_2.size()];
+                        copy(temp_2.begin(), temp_2.end(), temp_1);
+                        v.push_back(temp_1);
+                    }
+                    v.push_back('\0');
+                    ptr = &v[0];
+                } else {
+                    char *temp_1 = new char[temp.size() + 1];
+                    copy(temp.begin(), temp.end(), temp_1);
+                    temp_1[temp.size()] = '\0';
+                    v.push_back(temp_1);
+                    ptr = &v[0];
+                }
+                pid = fork();
+                if(pid == -1) {
+                    return -1;
+                }
+                if(pid == 0) {
+                    if(execvp(ptr[0], ptr) == -1) {
+                        perror("Execvp Failed");
+                    }
+                    exit(1);
+                }
+                if(pid > 0) {
+                    if(wait(0) == -1) {
+                        perror("Wait");
+                        exit(1);
+                    }
+                }
+                ptr = NULL;
+                v.clear();*/
+            //}
